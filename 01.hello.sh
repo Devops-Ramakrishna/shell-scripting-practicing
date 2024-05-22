80035e8b3732e2acf9d5cedadb316c6502fc9fb0
Devops-Ramakrishna <gamparamakrishna999@gmail.com>
2024-05-22 19:47:33 +0530

    practicing

--- 01.hello.sh
+++ 01.hello.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
 
+#hat is shell?
+
+#A unix shell program interpretes user commands which are directly enter by user 
+#or which can be read from a file is called shell or shell program
+
 cat /etc/shells
