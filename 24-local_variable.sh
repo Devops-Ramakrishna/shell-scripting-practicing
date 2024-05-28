47d0b2c66d369cb78badf7a00e0e946e2e729990
Devops-Ramakrishna <gamparamakrishna999@gmail.com>
2024-05-28 21:31:02 +0530

    practicing

---
+++ 24-local_variable.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+function print(){
+    name=$1
+        echo " the name is $name"
+}
+print max
+echo " foo "
+
+echo " ============================= "
+
+function print(){
+    name=$1
+        echo " the name is $name"
+}
+name="tom"
+echo " the name is $name"
+print max
+echo " foo "
+
+echo " ============================ "
+
+function print(){
+    local name=$1
+        echo " the name is $name"
+}
+name="tom" # Gobal variable
+echo " the name is $name : Before"
+
+print max # local variable
+
+echo " the name is $name : after"
+
+echo " foo "
+
+
+echo " ============================= "
