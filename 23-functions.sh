ba00ec3900eedb8269dae759892338e0f750aabe
Devops-Ramakrishna <gamparamakrishna999@gmail.com>
2024-05-28 21:16:05 +0530

    practicing

---
+++ 23-functions.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+# Using functions to perform repetitive tasks is an excellent way to create code reuse.
+
+# To declare a function, simply use the following syntax −
+
+# function_name () { 
+#   list of commands
+# }
+
+echo " ==================== "
+
+#!/bin/sh
+
+# Define your function here
+function Hello () {
+   echo "Hello World"
+}
+
+# Invoke your function
+Hello
+
+echo " ====================== "
+
+function Hello () {
+   echo "Hello World"
+}
+quit(){
+    exit
+}
+
+echo " ====================== "
