229429e578dac7ba1f24e3fad3a20b42598b6247
Devops-Ramakrishna <gamparamakrishna999@gmail.com>
2024-05-29 11:07:02 +0530

    practicing

---
+++ 27-signal_traps.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+echo " pid is $$ "
+while (( COUNT < 10 ))
+do 
+    sleep 10
+    (( COUNT++ ))
+    echo " $COUNT "
+done
+exit 0
+
+echo " ============================== "
+
+trap " echo exit command is detected "
+echo " hello world "
+exit 0
+
+echo " ============================= "
+
+trap " echo exit command is detected " 0 # 0 is success exit signal
+echo " pid is $$ "
+while (( COUNT < 10 ))
+do 
+    sleep 10
+    (( COUNT++ ))
+    echo " $COUNT "
+done
+exit 0
+
+echo " ============================== "
+
+file=file.txt
+trap ' rm -f $file && echo file detected; exit " 0 2 15
+
+
+
+
+
+
+
+
+
+
+
+
